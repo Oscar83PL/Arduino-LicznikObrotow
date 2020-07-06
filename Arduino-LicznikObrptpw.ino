63a9db5c9192f921dd969c40656f56ea933776e9
Oscar83PL <osa_g@o2.pl>
2020-07-06 15:21:50 +0200

    first commit

---
+++ Arduino-LicznikObrptpw.ino
@@ -0,0 +1,58 @@
+
+#include <Wire.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h"
+
+// 0X3C+SA0 - 0x3C or 0x3D
+#define I2C_ADDRESS 0x3C
+
+// Define proper RST_PIN if required.
+#define RST_PIN -1
+
+SSD1306AsciiWire oled;
+
+uint32_t Ttime, dTtime;            // integer for storing total time of a cycle
+float frequency;        //storing frequency
+float obroty;
+const int buttonPin = 2;     // the number of the pushbutton pin
+uint32_t screenupdateTime = micros();;
+void setup() {
+  Wire.begin();
+  
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+
+  oled.begin(&Adafruit128x32, I2C_ADDRESS, RST_PIN);
+  oled.setFont(Adafruit5x7);
+  oled.set1X();
+}
+
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+uint32_t startPulse, stopPulse;
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState != lastButtonState) {
+    if (buttonState == HIGH) {
+        stopPulse = micros();
+        Ttime = stopPulse - startPulse;
+        startPulse = micros();
+  
+        frequency=1000000.0/Ttime;    //getting frequency with Ttime is in Micro seconds
+        obroty = frequency * 60.0;
+
+      } else {
+      ;
+    }
+  }
+  lastButtonState = buttonState;
+
+    if (micros() > screenupdateTime + 1000000) {
+      oled.clear();
+      oled.println(Ttime);
+      oled.println(frequency);
+      oled.println(obroty);
+      screenupdateTime = micros();
+    }
+}
